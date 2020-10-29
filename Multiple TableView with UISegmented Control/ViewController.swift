081f2746692b3d431629adde02860ef81d238cec
iAdwait <adwaitbarkale@gmail.com>
2020-10-29 10:42:35 +0530

    Initial Commit

---
+++ Multiple TableView with UISegmented Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple TableView with UISegmented Control
+//
+//  Created by Adwait Barkale on 29/10/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
